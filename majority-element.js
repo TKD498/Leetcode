e9496c0f6a82278c7cfd0e84f77765f729137347
Tate <driggskingtate@gmail.com>
2025-07-29 09:04:43 -0700

    feat(majority-element) completed Majority Element Leetcode problem in O(n^2) time)

---
+++ majority-element.js
@@ -0,0 +1,28 @@
+((nums = [3, 3, 4]), (nums1 = [2, 2, 1, 1, 1, 2, 2]));
+
+var majorityElement = function (nums) {
+  let numbersCount = {};
+
+  for (const num of nums) {
+    if (!numbersCount[num]) {
+      //if variable does not exist, create it
+      numbersCount[num] = 1;
+    } else {
+      //if variable exists, increment it
+      numbersCount[num] += 1;
+    }
+  }
+
+  let majorityNumber = 0;
+  let highestCount = 0;
+  for (const element in numbersCount) {
+    console.log(numbersCount[element]);
+    if (numbersCount[element] > highestCount) {
+      highestCount = numbersCount[element];
+      majorityNumber = element;
+    }
+  }
+  return Number(majorityNumber);
+};
+
+console.log(majorityElement(nums));
